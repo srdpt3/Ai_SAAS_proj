04beb3df38efb218ee1cf30cee441dcb0e38fd6e
Dustin Yang <dustinyang@DustinucBookPro.lan>
2023-10-12 22:13:31 -0400

    update

--- src/app/[filename]/page.js
+++ src/app/[filename]/page.js
@@ -1,6 +1,12 @@
+"use client";
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function FilePage({ params }) {
   const filename = params.filename;
+  useEffect(() => {
+    axios.get("/api/transcribe?filename=" + filename);
+  }, [filename]);
   console.log(filename);
   return <div>{filename}</div>;
 }
-ㄴㅇㄹㅁㄴㅇㄹ;
